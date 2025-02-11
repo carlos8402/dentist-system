fe6a3dd163bf60053046237a5b08b8f765b99fba
carlos8468 <carlos_eduardo8468@hotmail.com>
2025-02-11 14:40:22 -0300

    terceiro commit correcao imagens

--- src/routes/differential/differential.tsx
+++ src/routes/differential/differential.tsx
@@ -1,7 +1,11 @@
 export function Differential() {
   return (
     <div className="md:flex bg-red-100 h-full py-8 px-4 md:gap-4">
-      <img src="img\diferencial.jpg" alt="diferencial" className="w-full md:min-w-[400px] md:h-[350px] lg:min-w-[600px] lg:h-[470px]  md:pt-6 md:sticky md:top-0" />
+      <img
+        src="img\diferencial.jpg"
+        alt="diferencial"
+        className="w-full md:min-w-[400px] md:h-[350px] lg:min-w-[600px] lg:h-[470px]  md:pt-6 md:sticky md:top-0"
+      />
       <div className="">
         <h1 className="text-3xl  text-amber-700 pt-4">DIFERENCIAL</h1>
         <h3 className="text-3xl font-bold text-amber-800 ">
@@ -21,12 +25,13 @@ export function Differential() {
           conclusão do tratamento.
         </p>
         <p className="text-lg pt-3">
-        Através de um trabalho responsável e uma comunicação única, eficiente e 
-        diferenciada, nos realizamos em poder ver a transformação do sorriso e 
-        da autoestima de cada paciente.
+          Através de um trabalho responsável e uma comunicação única, eficiente
+          e diferenciada, nos realizamos em poder ver a transformação do sorriso
+          e da autoestima de cada paciente.
         </p>
         <p className="text-lg pt-3">
-        Para agendar uma consulta e viver essa renovação, entre em contato comigo! Será um prazer cuidar de você.
+          Para agendar uma consulta e viver essa renovação, entre em contato
+          comigo! Será um prazer cuidar de você.
         </p>
       </div>
     </div>
